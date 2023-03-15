cd9cc90ff9335ba0f0ce3f465498e68d487f85f0
Scott Schindler <scottschindler29@gmail.com>
2023-03-15 13:43:30 -0500

    deploy init

---
+++ pages/api/getAnswer.ts
@@ -0,0 +1,42 @@
+import { OpenAI } from "langchain/llms";
+import { VectorDBQAChain } from "langchain/chains";
+import { HNSWLib } from "langchain/vectorstores";
+import { OpenAIEmbeddings } from "langchain/embeddings";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { NextApiRequest, NextApiResponse } from "next";
+
+import * as fs from "fs";
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  return new Promise<void>(async (resolve) => {
+    try {
+      const model = new OpenAI({
+        openAIApiKey: process.env.OPEN_AI_KEY,
+      });
+      const text = fs.readFileSync("text.txt", "utf8");
+      const textSplitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 1000,
+      });
+
+      const docs = await textSplitter.createDocuments([text]);
+      const vectorStore = await HNSWLib.fromDocuments(
+        docs,
+        new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPEN_AI_KEY,
+        })
+      );
+      const chain = VectorDBQAChain.fromLLM(model, vectorStore);
+      const result = await chain.call({
+        query: req.body.question,
+      });
+      res.status(200).json(result);
+    } catch (error) {
+      console.log(error);
+      res.status(500).end();
+      return resolve();
+    }
+
+    res.status(405).end();
+    return resolve();
+  });
+}
